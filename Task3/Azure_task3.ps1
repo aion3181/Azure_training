0ad0637b15b64d0535bea1d2ea1f46a7555fb321
aion3181 <aion3181@gmail.com>
2018-01-19 01:59:12 +0300

    task 3

---
+++ Task3/Azure_task3.ps1
@@ -0,0 +1,15 @@
+﻿Login-AzureRMAccount
+Set-AzureRmContext –SubscriptionId bf8f23dc-8f88-417d-8b3b-da7cc4c74793
+$ResourceGroupLocation = "West Europe"
+$location = "West Europe"
+$TemplateFile = ".\Environment.json"
+$ResourceGroupName = "azuretraining"
+
+New-AzureRmResourceGroup -Name $envPrefix -Location $ResourceGroupLocation
+New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName `
+                                   -TemplateFile $TemplateFile `
+                                   -location $location `
+                                   -Force -Verbose
+
+
+
