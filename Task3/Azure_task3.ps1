5a68b5021e47c0bf59d52210485cb7842742c54e
aion3181 <aion3181@gmail.com>
2018-01-19 11:44:03 +0300

    Update Azure_task3.ps1

--- Task3/Azure_task3.ps1
+++ Task3/Azure_task3.ps1
@@ -1,15 +1,20 @@
-﻿Login-AzureRMAccount
+$ScriptPath = Split-Path -Parent $PSCommandPath
+
+Login-AzureRMAccount
 Set-AzureRmContext –SubscriptionId bf8f23dc-8f88-417d-8b3b-da7cc4c74793
 $ResourceGroupLocation = "West Europe"
-$location = "West Europe"
-$TemplateFile = ".\Environment.json"
+$resLocation = "West Europe"
+$TemplateFile = "$ScriptPath\Environment.json"
 $ResourceGroupName = "azuretraining"
 
-New-AzureRmResourceGroup -Name $envPrefix -Location $ResourceGroupLocation
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation
 New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName `
                                    -TemplateFile $TemplateFile `
-                                   -location $location `
+                                   -resLocation $resLocation `
                                    -Force -Verbose
+
+
+
 
 
 
