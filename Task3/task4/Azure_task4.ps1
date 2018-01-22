54cb881a661ae9b3d034bbbefdd84c37fe19245c
aion3181 <aion3181@gmail.com>
2018-01-22 17:28:45 +0300

    Add files via upload

---
+++ Task3/task4/Azure_task4.ps1
@@ -0,0 +1,17 @@
+﻿$ScriptPath = Split-Path -Parent $PSCommandPath
+
+Login-AzureRMAccount
+Set-AzureRmContext –SubscriptionId bf8f23dc-8f88-417d-8b3b-da7cc4c74793
+$ResourceGroupLocation = "West Europe"
+$resLocation = "West Europe"
+$TemplateFile = "$ScriptPath\Environment.json"
+$ResourceGroupName = "azuretraining"
+
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation
+New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName `
+                                   -TemplateFile $TemplateFile `
+                                   -resLocation $resLocation `
+                                   -Force -Verbose
+
+
+
