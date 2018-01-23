48a85061913f121edacc07a41003148c35f115a7
aion3181 <aion3181@gmail.com>
2018-01-23 18:41:12 +0300

    Update Azure_task4.ps1

--- task4/Azure_task4.ps1
+++ task4/Azure_task4.ps1
@@ -1,17 +1,56 @@
-﻿$ScriptPath = Split-Path -Parent $PSCommandPath
+$ScriptPath = Split-Path -Parent $PSCommandPath
 
 Login-AzureRMAccount
 Set-AzureRmContext –SubscriptionId bf8f23dc-8f88-417d-8b3b-da7cc4c74793
 $ResourceGroupLocation = "West Europe"
 $resLocation = "West Europe"
+$TemplateFileSA = "$ScriptPath\Templates\SA.json"
 $TemplateFile = "$ScriptPath\Environment.json"
 $ResourceGroupName = "azuretraining"
+$storageAccountName = "$ResourceGroupName" + "sa2"
+$ContainerName = "Task4"
+$ConfigurationPath = "$ScriptPath\IIS.ps1"
 
 New-AzureRmResourceGroup -Name $ResourceGroupName -Location $ResourceGroupLocation
+
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName `
+                                   -TemplateFile $TemplateFileSA `
+                                   -storageAccountName $storageAccountName `
+                                   -storageAccountLocation $resLocation `
+                                   -Force -Verbose
+
+
+$moduleURL = Publish-AzureRmVMDscConfiguration -ConfigurationPath $ConfigurationPath `
+                                    -ResourceGroupName $ResourceGroupName `
+                                    -StorageAccountName $storageAccountName `
+                                    -Force
+                                    #–ContainerName $ContainerName
+$moduleURL
+
+$StorageAccountKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $ResourceGroupName `
+                                    -Name $storageAccountName)[0].Value
+
+
+$storageContext = New-AzureStorageContext –StorageAccountName $storageAccountName `
+                                    -StorageAccountKey $StorageAccountKey
+
+$sasToken = New-AzureStorageContainerSASToken –Name $ContainerName `
+                                    –Context $storageContext –Permission r
+
+
+
+
 New-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName `
                                    -TemplateFile $TemplateFile `
                                    -resLocation $resLocation `
+                                   -modulesUrl $moduleURL `
+                                   -sasToken $sasToken `
                                    -Force -Verbose
 
 
 
+<#
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $ResourceGroupName `
+  -TemplateFile $TemplateFile -Debug
+#>
