36b0f996edaec1f15742f165d1b1d889797485b9
aion3181 <aion3181@gmail.com>
2018-01-23 18:41:42 +0300

    Add files via upload

---
+++ task4/IISback.ps1
@@ -0,0 +1,46 @@
+﻿Configuration MyDscConfiguration {
+    Import-DscResource -Module PSDesiredStateConfiguration
+    Import-DscResource -Module xWebAdministration
+
+    param(
+            [string[]]$MachineName
+        )
+    Node $MachineName {
+        {
+        # Install the IIS role
+        WindowsFeature IIS
+        {
+            Ensure       = 'Present'
+            Name         = 'Web-Server'
+        }
+
+        # Install the ASP .NET 4.5 role
+        WindowsFeature AspNet45
+        {
+            Ensure       = 'Present'
+            Name         = 'Web-Asp-Net45'
+
+        }
+
+        # Stop the default website
+        xWebsite DefaultSite 
+        {
+            Ensure       = 'Present'
+            Name         = 'Default Web Site'
+            State        = 'Started'
+            PhysicalPath = 'C:\inetpub\wwwroot'
+            BindingInfo     = MSFT_xWebBindingInformation  
+            {  
+                Protocol              = "HTTP" 
+                Port                  = 8080
+            }
+            DependsOn    = '[WindowsFeature]IIS'
+
+        }
+
+
+    }
+
+    }
+
+}
