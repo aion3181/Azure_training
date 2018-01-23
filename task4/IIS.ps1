b73dbb39582de76193a89c6a595188f63e7469d8
aion3181 <aion3181@gmail.com>
2018-01-24 01:34:55 +0300

    Update IIS.ps1

--- task4/IIS.ps1
+++ task4/IIS.ps1
@@ -1,22 +1,60 @@
-﻿Configuration MyDscConfiguration
-{
-  param ($MachineName)
+Configuration MyDscConfiguration {
+    Import-DscResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName WebAdministration
 
-  Node $MachineName
-  {
-    #Install the IIS Role
-    WindowsFeature IIS
-    {
-      Ensure = “Present”
-      Name = “Web-Server”
+    param(
+            [string[]]$MachineName
+        )
+    Node $MachineName {
+        {
+        # Install the IIS role
+        WindowsFeature IIS
+        {
+            Ensure       = 'Present'
+            Name         = 'Web-Server'
+
+        }
+
+        # Install the ASP .NET 4.5 role
+        WindowsFeature AspNet45
+        {
+            Ensure       = 'Present'
+            Name         = 'Web-Asp-Net45'
+
+        }
+
+        WindowsFeature WebManagementConsole
+        {
+          Name = "Web-Mgmt-Console"
+          Ensure = "Present"
+        }
+
+        WindowsFeature WebManagementService
+        {
+          Name = "Web-Mgmt-Service"
+          Ensure = "Present"
+        }
+
+        # default website
+        xWebsite DefaultSite 
+        {
+            Ensure       = 'Present'
+            Name         = 'Default Web Site'
+            State        = 'Started'
+            PhysicalPath = 'C:\inetpub\wwwroot'
+            BindingInfo     = MSFT_xWebBindingInformation  
+            {  
+                Protocol              = "HTTP" 
+                Port                  = 8080
+            }
+            DependsOn    = '[WindowsFeature]IIS'
+
+        }
+
+
     }
 
-    #Install ASP.NET 4.5
-    WindowsFeature ASP
-    {
-      Ensure = “Present”
-      Name = “Web-Asp-Net45”
     }
 
-  }
-} 
+
+}
