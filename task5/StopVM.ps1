4084e3a662f790ca1ce23d64182d522001d50b8b
aion3181 <aion3181@gmail.com>
2018-02-06 08:12:42 +0300

    Add files via upload

---
+++ task5/StopVM.ps1
@@ -0,0 +1,34 @@
+﻿Workflow Stop-AzureVM
+{
+    Param 
+    (    
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] 
+        [String] 
+        $AzureSubscriptionId, 
+        [Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] 
+        [String] 
+        $AzureVMList="All"
+    )
+
+    Node WebServer
+    {
+
+        WindowsFeature IIS
+        {
+            Ensure = 'Present'
+            Name = 'Web-Server'
+            IncludeAllSubFeature = $true
+
+        }
+    }
+
+    Node NotWebServer
+    {
+        WindowsFeature IIS
+        {
+            Ensure = 'Absent'
+            Name = 'Web-Server'
+
+        }
+    }
+}
