c94046f4189bfb8cc17a522abf1c7efc56c059f2
aion3181 <aion3181@gmail.com>
2018-02-07 16:24:49 +0300

    Add files via upload

---
+++ task5/DSC/AutoDSCPull.ps1
@@ -0,0 +1,44 @@
+﻿[DscLocalConfigurationManager()]
+configuration MyDscConfiguration
+{
+    param
+    (
+        [Parameter(Mandatory=$True)]
+        [String]$RegistrationUrl,
+
+        [Parameter(Mandatory=$True)]
+        [String]$RegistrationKey,
+
+        [Int]$RefreshFrequencyMins,
+            
+        [Int]$ConfigurationModeFrequencyMins,
+            
+        [String]$ConfigurationMode,
+            
+        [String]$NodeConfigurationName
+    )
+
+    Node localhost
+    {
+        Settings
+        {
+            RefreshFrequencyMins = $RefreshFrequencyMins
+            RefreshMode = "PULL"
+            ConfigurationMode = $ConfigurationMode
+            ConfigurationModeFrequencyMins = $ConfigurationModeFrequencyMins
+        }
+
+        ConfigurationRepositoryWeb AzureAutomationDSC
+        {
+            ServerUrl = $RegistrationUrl
+            RegistrationKey = $RegistrationKey
+            ConfigurationNames = @($NodeConfigurationName)
+        }
+
+        ResourceRepositoryWeb AzureAutomationDSC
+        {
+            ServerUrl = $RegistrationUrl
+            RegistrationKey = $RegistrationKey
+        }
+    }
+}
